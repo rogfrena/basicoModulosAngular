9f78ca0d856f0d7a6b6e1e59a25523d9df84c530
rogfrena <rogfren.angulo@gmail.com>
2020-06-04 18:28:11 -0400

    final a produccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ import { ServiciosComponent } from './servicios/servicios.component';
 import { UserComponent } from './user/user.component';
 import { TipoCuentaComponent } from './tipo-cuenta/tipo-cuenta.component';
 import { IngresarComponent } from './ingresar/ingresar.component';
+import { InicioComponent } from './shared/inicio/inicio.component';
 
 
 const routes: Routes = [
+  {path: '', component: InicioComponent},
   {path: 'entrada', component: EntradaComponent},
   {path: 'salida', component: SalidaComponent},
   {path: 'servicios', component: ServiciosComponent},
